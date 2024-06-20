944990b49cfe1b8136c61a19f01471f7c65b1943
EnusoiuVirgil <enusoiuvergil@gmail.com>
2024-06-20 18:24:37 +0300

    veterinary service and controller

---
+++ src/main/java/org/example/petwell_clinic/controller/VeterinaryController.java
@@ -0,0 +1,46 @@
+package org.example.petwell_clinic.controller;
+
+import org.example.petwell_clinic.entity.Veterinary;
+import org.example.petwell_clinic.service.VeterinaryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/veterinary")
+public class VeterinaryController {
+    private final VeterinaryService veterinaryService;
+    public VeterinaryController (VeterinaryService veterinaryService) {
+        this.veterinaryService=veterinaryService;
+    }
+    @PostMapping("/add")
+    public void addCar(@RequestBody Veterinary veterinary){
+        veterinaryService.addVeterinary(veterinary);
+    }
+
+    @GetMapping("/getAll")
+    public List<Veterinary> getAllVeterinary() {
+        return veterinaryService.getAllVeterinary();
+    }
+
+    @DeleteMapping("/deleteId/{veterinaryId}")
+    public void deleteVeterinaryById(@PathVariable(name = "veterinaryId") long id){
+        veterinaryService.deleteVeterinaryById(id);
+    }
+
+    @PutMapping("/update")
+    public void updateVeterinary(@RequestBody Veterinary veterinary) {
+        veterinaryService.updateVeterinaryByObject(veterinary);
+    }
+
+    @GetMapping("/getId/{veterinaryId}")
+    public Optional<Veterinary> getVeterinaryById(@PathVariable(name="veterinaryId") long id) {
+        return veterinaryService.getVeterinaryByID(id);
+    }
+
+    @GetMapping("getName/{veterinaryName}")
+    public Optional<Veterinary> getVeterinaryByName(@PathVariable(name="veterinaryName") String name){
+        return veterinaryService.getVeterinaryByName(name);
+    }
+}
---
+++ src/main/java/org/example/petwell_clinic/service/VeterinaryService.java
@@ -0,0 +1,54 @@
+package org.example.petwell_clinic.service;
+
+import org.example.petwell_clinic.entity.Appointment;
+import org.example.petwell_clinic.entity.Schedule;
+import org.example.petwell_clinic.entity.Veterinary;
+import org.example.petwell_clinic.repository.VeterinaryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VeterinaryService {
+
+    private final VeterinaryRepository veterinaryRepository;
+    public VeterinaryService(VeterinaryRepository veterinaryRepository) {
+        this.veterinaryRepository=veterinaryRepository;
+    }
+
+    public void addVeterinary(Veterinary veterinary){
+            veterinaryRepository.save(veterinary);
+    }
+
+    public List<Veterinary> getAllVeterinary(){
+            return veterinaryRepository.findAll();
+    }
+
+    public String deleteVeterinaryById(long id) {
+        Optional <Veterinary> veterinaryToDelete=veterinaryRepository.findById(id);
+        if (veterinaryToDelete.isPresent()){
+            veterinaryRepository.deleteById(id);
+            return ("veterinary deleted");
+        }
+        return ("veterinary not found");
+    }
+
+    public String updateVeterinaryByObject(Veterinary veterinary){
+        Optional <Veterinary> veterinaryToUpdate=veterinaryRepository.findById(veterinary.getVeterinary_id());
+        if(veterinaryToUpdate.isPresent()) {
+            veterinaryRepository.save(veterinary);
+            return("veterinary updated");
+        }
+        return ("veterinary not found");
+    }
+
+    public Optional <Veterinary> getVeterinaryByID (long id){
+        return (veterinaryRepository.findById(id));
+    }
+
+    public Optional <Veterinary> getVeterinaryByName(String name) {
+        return (veterinaryRepository.findAll().stream().filter(veterinary -> veterinary.getName().equals(name))).findFirst();
+///  de corectat
+    }
+}
